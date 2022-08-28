040789e6f737f256dc87f287c7edd3b466942e97
lalua456 <lalua456@gmail.com>
2022-08-28 17:50:27 +0900

    add: argsparser

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
     parser.add_argument("--batch", type=int, default=32)
     parser.add_argument("--lr", type=float, default=1e-4)
     parser.add_argument("--eps", type=float, default=1e-8)
-    parser.add_argument("--epochs", type=int, default=1)
+    parser.add_argument("--epochs", type=int, default=1000)
     parser.add_argument("--pause", type=int, default=0)
     parser.add_argument('--enc_input_size', type=int, default=512)
     parser.add_argument('--enc_latent_dim', type=int, default=512)
